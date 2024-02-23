9f13f48bfeb3008050e1b48a61bd0d2ecbc9a06d
Yasin-Ozdemir <yasinozdemirgs37@gmail.com>
2024-02-23 23:33:46 +0300

    Upcoming ekranı tamamlandı.

---
+++ NetflixClone/Views/UpcomingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UpcomingTableViewCell.swift
+//  NetflixClone
+//
+//  Created by Yasin Özdemir on 23.02.2024.
+//
+
+import UIKit
+
+class UpcomingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
